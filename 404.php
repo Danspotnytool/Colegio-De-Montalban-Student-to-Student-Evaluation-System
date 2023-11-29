6f389b05d1365677752cda14eb9430128e82a880
Danspotnytool <firedantsm@gmail.com>
2023-11-29 23:47:28 +0800

    php

---
+++ 404.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Error: 404 - Colegio de Montalban Student to Student Evaluation System</title>
+
+    <link rel="shortcut icon" href="./assets/Favicon.png" type="image/x-icon">
+
+    <link rel="stylesheet" href="./fonts/font-face.css">
+
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="stylesheet" href="./css/404.css">
+</head>
+
+<body>
+    <div id="main">
+        <img src="./assets/404.png" alt="404">
+        <h2>Oops! Page not found.</h2>
+        <p>Sorry, the page you're looking for doesn't exist.</p>
+        <a href="index.php" class="button sub">
+            <p><b>Back to Homepage</b></p>
+        </a>
+    </div>
+</body>
+
+</html>
